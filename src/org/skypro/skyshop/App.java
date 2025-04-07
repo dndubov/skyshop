cdcf2533b92f1e3f31e29e83ada6281acb8491bb
Dmitriy <dmitriyndubov@gmail.com>
2025-04-07 16:04:10 +0500

    Task "OOP. Inheritance. Abstract classes" - completed

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -1,23 +1,25 @@
 package org.skypro.skyshop;
 
 import org.skypro.skyshop.basket.ProductBasket;
-import org.skypro.skyshop.product.Product;
+import org.skypro.skyshop.product.*;
 
 public class App {
     public static void main(String[] args) {
         // 1. Создаем корзину
         ProductBasket basket = new ProductBasket();
 
-        // 2. Создаем тестовые продукты
-        Product milk = new Product("Молоко", 80);
-        Product bread = new Product("Хлеб", 50);
-        Product eggs = new Product("Яйца", 120);
+        // 2. Создаем тестовые продукты разных типов
+        SimpleProduct milk = new SimpleProduct("Молоко", 80);
+        SimpleProduct bread = new SimpleProduct("Хлеб", 50);
+        DiscountedProduct eggs = new DiscountedProduct("Яйца", 120, 10);
+        FixPriceProduct cheese = new FixPriceProduct("Сыр");
 
         // 3. Добавляем продукты в корзину
         System.out.println("=== Добавление продуктов ===");
         basket.addProduct(milk);
         basket.addProduct(bread);
         basket.addProduct(eggs);
+        basket.addProduct(cheese);
 
         // 4. Выводим содержимое корзины
         System.out.println("\n=== Содержимое корзины ===");
@@ -28,12 +30,7 @@ public class App {
         basket.printProductPresence("Хлеб");
         basket.printProductPresence("Колбаса");
 
-        // 6. Пытаемся добавить в заполненную корзину
-        System.out.println("\n=== Попытка переполнения ===");
-        basket.addProduct(new Product("Сыр", 200));
-        basket.addProduct(new Product("Йогурт", 60));
-
-        // 7. Очищаем корзину
+        // 6. Очищаем корзину
         System.out.println("\n=== Очистка корзины ===");
         basket.clear();
         basket.printContents();
