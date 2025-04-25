52776e3d36c440308ce7a411be0ff0b15fefd506
Dmitriy <dmitriyndubov@gmail.com>
2025-04-25 12:42:27 +0500

    Task "Exceptions in Java" - completed

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -2,38 +2,39 @@ package org.skypro.skyshop;
 
 import org.skypro.skyshop.product.*;
 import org.skypro.skyshop.search.SearchEngine;
-import org.skypro.skyshop.product.Article;
-
-import java.util.Arrays;
+import org.skypro.skyshop.search.BestResultNotFound;
 
 public class App {
     public static void main(String[] args) {
         // Создаем поисковый движок
         SearchEngine searchEngine = new SearchEngine(10);
 
-        // Создаем товары
-        SimpleProduct milk = new SimpleProduct("Молоко", 80);
-        DiscountedProduct eggs = new DiscountedProduct("Яйца", 120, 10);
-        FixPriceProduct cheese = new FixPriceProduct("Сыр");
+        // Пример создания продуктов
+        try {
+            SimpleProduct milk = new SimpleProduct("Молоко", 80);
+            DiscountedProduct eggs = new DiscountedProduct("Яйца", 120, 10);
+            FixPriceProduct cheese = new FixPriceProduct("Сыр");
 
-        // Добавляем товары в движок
-        searchEngine.add(milk);
-        searchEngine.add(eggs);
-        searchEngine.add(cheese);
+            searchEngine.add(milk);
+            searchEngine.add(eggs);
+            searchEngine.add(cheese);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Ошибка при создании продукта: " + e.getMessage());
+        }
 
         // Создаем статьи
         Article milkArticle = new Article("Скидки на молоко", "Как купить молоко по акции.");
         Article cheeseArticle = new Article("Сыр и его виды", "Все, что нужно знать о сыре.");
 
-        // Добавляем статьи в движок
         searchEngine.add(milkArticle);
         searchEngine.add(cheeseArticle);
 
         // Пример поиска
-        System.out.println("Результаты поиска по запросу 'молоко':");
-        System.out.println(Arrays.toString(searchEngine.search("молоко")));
-
-        System.out.println("Результаты поиска по запросу 'сыр':");
-        System.out.println(Arrays.toString(searchEngine.search("сыр")));
+        try {
+            Searchable bestMatch = searchEngine.findBestMatch("молоко");
+            System.out.println("Лучший результат: " + bestMatch.getStringRepresentation());
+        } catch (BestResultNotFound e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
